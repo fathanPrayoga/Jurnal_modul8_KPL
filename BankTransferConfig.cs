4462b89b7c1ffeaf5baec68f3b021cc3484b70a8
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-15 08:15:52 +0700

    aplikasi bank transfer menggunakan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_103022330070
+{
+    class BankTransferConfig
+    {
+       
+        public string lang { get; set; }
+        public Transfer transfer { get; set; }
+        public List<String> methods { get; set; }
+        public Confirmation confirmation { get; set; }
+
+
+        public class Transfer()
+        {
+            public int threshold { get; set; }
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+        }
+
+        public class Confirmation()
+        {
+            public string en { get; set; }
+            public string id { get; set; }
+        }
+
+        public BankTransferConfig LoadConfig(string filePath)
+        {
+            filePath = "bank_transfer_config.json";
+            String Json = File.ReadAllText(filePath);
+            var config = JsonSerializer.Deserialize<BankTransferConfig>(Json);
+            
+            return config;
+
+        } 
+    }
+}
+
+
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using System.Numerics;
+using modul8_103022330070;
+
+class Program
+{
+    public void Main(string[] args)
+    {
+        BankTransferConfig config = new BankTransferConfig();
+        int total;
+
+        if (config.lang == "en")
+        {
+            Console.WriteLine("please insert the amount of money to transfer: ");
+            int money = Convert.ToInt32(Console.ReadLine());
+
+            if (money <= config.transfer.threshold)
+            {
+                total = money * config.transfer.low_fee;
+            }
+            else
+            {
+                total = money * config.transfer.high_fee;
+            }
+
+            Console.WriteLine("Select transfer: ");
+            for(int i =  0; i < config.methods.Count; i++)
+            {
+                Console.WriteLine((i + 1) + config.methods[i]);
+            }
+            int metode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n Please type" + config.confirmation.en);
+
+            string confirm = Convert.ToString(Console.ReadLine());
+            if (confirm == )
+            
+        }
+    }
+}
